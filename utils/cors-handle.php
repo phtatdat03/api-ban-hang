523312fe5b5bd78565c17b60817a92ec45dea759
phtatdat03 <phamtatdat2703@gmail.com>
2025-01-11 01:48:19 +0700

    config cors

--- utils/cors-handle.php
+++ utils/cors-handle.php
@@ -5,18 +5,14 @@ function handleCors() {
     header("Content-Type: application/json; charset=UTF-8");
 
     header("Access-Control-Allow-Origin: *");
-    
+    // Các headers được phép
+    header("Access-Control-Allow-Headers: *");
     // Cho phép credentials (nếu bạn gửi cookies hoặc auth headers)
     header("Access-Control-Allow-Credentials: true");
-    
-    // Các headers được phép
-    header("Access-Control-Allow-Headers: Origin, Cache-Control: null, X-Requested-With: null, Content-Type: text/html, Accept, Authorization");
-    
     // Các methods được phép
     header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
     
 }
-
 
 handleCors();
 
